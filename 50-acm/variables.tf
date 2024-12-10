341beb90514cecd60e0dd24aaffe25c3c926bc38
Prudviraj <prudvivemula10@mail.com>
2024-12-10 13:03:12 +0530

    user profile

---
+++ 50-acm/variables.tf
@@ -0,0 +1,24 @@
+variable "project_name" {
+    default = "roboshop"
+}
+
+variable "environment" {
+    default = "dev"
+}
+
+variable "common_tags" {
+    default = {
+        Project = "roboshop"
+        Terraform = "true"
+        Environment = "dev"
+    }
+}
+
+
+variable "zone_name" {
+    default = "paws81s.online"
+}
+
+variable "zone_id" {
+    default = "Z01941381B3D89AUM26J0"
+}
